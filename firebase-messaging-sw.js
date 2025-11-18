91f02c0e36567eae7641f7985372fbd5f9d00504
roneymoveapp <roney.fnds@gmail.com>
2025-11-18 07:44:34 +0000

    feat: Initialize Firebase messaging in SW
    Updates the Firebase Messaging Service Worker to directly initialize Firebase with the configuration and simplifies the background message handling logic.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,7 @@
-// Import Firebase compatibility scripts
 importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
 
-// Firebase configuration provided by the user
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyCTwvoS5pM-f-9qZ8gQgg727OXHpjdoLmg",
   authDomain: "app-move-motorista.firebaseapp.com",
   projectId: "app-move-motorista",
@@ -11,47 +9,30 @@ const firebaseConfig = {
   messagingSenderId: "746812406976",
   appId: "1:746812406976:web:110a4c6406f67140b34125",
   measurementId: "G-QWHJM4S6NX"
-};
+});
 
-// Initialize Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-// Retrieve an instance of Firebase Messaging to handle background messages
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Mensagem recebida em segundo plano: ', payload);
-
-  // Customize notification here
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = payload.notification?.title || "Nova mensagem";
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/vite.svg' // You can change this to your app icon
+    body: payload.notification?.body,
+    icon: "/vite.svg"
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// Handle notification click event
-self.addEventListener('notificationclick', (event) => {
-  console.log('[firebase-messaging-sw.js] Notificação clicada.');
+self.addEventListener("notificationclick", (event) => {
   event.notification.close();
 
-  // This focuses on an existing tab if it's open or opens a new one
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
-      // Check if there is a window open with the target URL
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
       for (const client of clientList) {
-        if (client.url === self.origin + '/' && 'focus' in client) {
-          return client.focus();
-        }
+        if ("focus" in client) return client.focus();
       }
-      // If no window is found, open a new one
-      if (clients.openWindow) {
-        return clients.openWindow('/');
-      }
+
+      return clients.openWindow("/");
     })
   );
 });
